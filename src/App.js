1112f173dd94654b241f5caf0ea835ad8a1b53bb
TheDreamatorium <davidseesahaye@gmail.com>
2022-03-24 23:23:07 +0400

    build meme component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Meme from "./components/Meme";
 import "./style.scss";
 
 export default function App() {
     return (
         <div className="app">
             <Header/>
+            <Meme/>
         </div>
     )
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,17 @@
+export default function Meme() {
+    return (
+        <main>
+            <form>
+                <input 
+                    type="text"
+                    placeholder="top text"
+                />
+                <input 
+                    type="text"
+                    placeholder="bottom text"
+                />
+                <button>Get a new meme image</button>
+            </form>
+        </main>
+    )
+}
